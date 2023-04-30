41f31096f2dacfea74846c42778fb1f0b3877803
@ugurkocde <ugur.koc@protonmail.com>
2023-04-30 16:29:31 +0200

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { DocumentContext, DocumentInitialProps } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
