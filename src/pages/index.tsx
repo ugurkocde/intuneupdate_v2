9b250d61ed9b226f37e399298fe40fed21cdb7f7
@ugurkocde <ugur.koc@protonmail.com>
2023-04-30 15:33:37 +0200

    fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -468,56 +468,60 @@ const Home: NextPage = () => {
         </div>
       )}
       <ToastContainer position={toast.POSITION.TOP_CENTER} />
-      <footer className="fixed-footer mt-12 border-t border-gray-200 bg-white py-4 shadow shadow-lg">
-        <div className="container mx-auto px-4">
-          <div className="flex items-center justify-between space-x-4">
-            <div className="flex items-center space-x-4">
-              <a
-                href="https://twitter.com/IntuneUpdate"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Twitter"
-              >
-                <BsTwitter className="text-xl hover:text-blue-500" />
-              </a>
-              <a
-                href="https://linkedin.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="GitHub"
-              >
-                <BsGithub className="text-xl hover:text-blue-700" />
-              </a>
-            </div>
+      <div>
+        {" "}
+        <footer className="fixed-footer mt-12 border-t border-gray-200 bg-white py-4 shadow shadow-lg">
+          <div className="container mx-auto px-4">
+            <div className="flex items-center justify-between space-x-4">
+              <div className="flex items-center space-x-4">
+                <a
+                  href="https://twitter.com/IntuneUpdate"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Twitter"
+                >
+                  <BsTwitter className="text-xl hover:text-blue-500" />
+                </a>
+                <a
+                  href="https://linkedin.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="GitHub"
+                >
+                  <BsGithub className="text-xl hover:text-blue-700" />
+                </a>
+              </div>
 
-            <div className="text-s w-1/2 text-center font-semibold">
-              {" "}
-              <a>
-                Stay up-to-date on the latest Intune related news from industry
-                experts and Microsoft, with AI generated summaries and more.
-              </a>
-            </div>
-            <div className="flex items-center space-x-4">
-              <a
-                href="https://ugurkoc.de/privacy-policy/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Privacy Policy"
-              >
-                <MdPrivacyTip className="text-xl hover:text-blue-500" />
-              </a>
-              <a
-                href="https://ugurkoc.de/imprint/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Imprint"
-              >
-                <GrCompliance className="text-xl hover:text-blue-700" />
-              </a>
+              <div className="text-s md:test-xs text-center font-semibold md:w-1/2">
+                {" "}
+                <a>
+                  Stay up-to-date on the latest Intune related news from
+                  industry experts and Microsoft, with AI generated summaries
+                  and more.
+                </a>
+              </div>
+              <div className="flex items-center space-x-4">
+                <a
+                  href="https://ugurkoc.de/privacy-policy/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Privacy Policy"
+                >
+                  <MdPrivacyTip className="text-xl hover:text-blue-500" />
+                </a>
+                <a
+                  href="https://ugurkoc.de/imprint/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Imprint"
+                >
+                  <GrCompliance className="text-xl hover:text-blue-700" />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 };
