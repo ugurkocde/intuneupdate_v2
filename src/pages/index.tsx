df660b3872b50eb6712e9621045c517a1fb516f1
@ugurkocde <ugur.koc@protonmail.com>
2023-06-14 15:45:26 +0200

    delete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,9 @@ import React, { useEffect, useState, useMemo, useRef } from "react";
 import type { NextPage } from "next";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { MdPrivacyTip } from "react-icons/md";
-import { GrCompliance } from "react-icons/gr";
-
-import { BsTwitter } from "react-icons/bs";
 
 import AllCards from "~/components/AllCards";
 import Header from "~/components/Header";
-import Head from "next/head";
 import Footer from "~/components/Footer";
 
 const Home: NextPage = () => {
