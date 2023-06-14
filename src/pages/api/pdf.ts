e5e011eceeb2cc2f063ede4ad1bd21c7f87a87f1
@ugurkocde <ugur.koc@protonmail.com>
2023-06-14 15:16:31 +0200

    asd

--- src/pages/api/pdf.ts
+++ src/pages/api/pdf.ts
@@ -1,7 +1,6 @@
-// Path: src\pages\api\pdf.ts
-
 import { NextApiRequest, NextApiResponse } from "next";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core"; // use puppeteer-core instead of puppeteer
+import chromium from "chrome-aws-lambda"; // add this line
 
 export default async function handler(
   req: NextApiRequest,
@@ -14,23 +13,32 @@ export default async function handler(
     return;
   }
 
+  let browser = null;
+
   try {
-    const browser = await puppeteer.launch();
+    // Use the chromium version from chrome-aws-lambda package and puppeteer-core
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+
     const page = await browser.newPage();
 
     await page.goto(url, { waitUntil: "networkidle2" });
 
     const pdf = await page.pdf({ format: "A4" });
 
-    await browser.close();
-
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader("Content-Length", pdf.length.toString());
     res.setHeader("Content-Disposition", `attachment; filename="blog.pdf"`);
     res.send(pdf);
   } catch (error) {
-    // Cast the error to the Error type
     const e = error as Error;
     res.status(500).send(`Error: ${e.message}`);
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
 }
